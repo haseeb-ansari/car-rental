7ffa4466a2d0feae34f6453718efd358527bd575
haseeb-ansari <98595823+haseeb-ansari@users.noreply.github.com>
2023-12-06 20:31:19 +0530

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vite'
+import * as path from "path";
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+// })
+
+
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@assets": path.resolve(`${__dirname}, "src/assets"`),
+    },
+  },
+});
